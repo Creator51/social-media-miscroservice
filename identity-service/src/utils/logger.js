8c335e25085969e1c1b2b0f0a34b01d42e21691a
Creator51 <gautamprod10@gmail.com>
2026-05-23 12:01:51 +0530

    Initial microservices commit

---
+++ identity-service/src/utils/logger.js
@@ -0,0 +1,25 @@
+const winston = require('winston')
+
+const logger = winston.createLogger({
+    level : process.env.NODE_ENV === 'production' ? 'info' : 'debug',
+    format : winston.format.combine(
+        winston.format.timestamp(),
+        winston.format.errors({stack : true}),
+        winston.format.splat(),
+        winston.format.json()
+
+    ),
+    defaultMeta : {service : 'identity-service'},
+    transports : [ // Transport transport as a delivery method for logs.
+        new winston.transports.Console({
+            format: winston.format.combine(
+                winston.format.colorize(),
+                winston.format.simple()
+            )
+        }),
+        new winston.transport.File({filename : 'error.log', level:'error'}),
+        new winston.transport.File({filename : 'combined.log'})
+    ]
+});
+
+module.exports = logger;

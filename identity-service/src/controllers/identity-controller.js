853137dff35d0edaaf16ca85691b3ffb328fb983
Creator51 <gautamprod10@gmail.com>
2026-05-25 16:20:16 +0530

    Registration is working

--- identity-service/src/controllers/identity-controller.js
+++ identity-service/src/controllers/identity-controller.js
@@ -1,89 +1,85 @@
-
 const generateTokens = require('../utils/generateToken')
 const logger = require('../utils/logger')
 const { validateRegistration } = require('../utils/validation')
+const User = require('../models/User')
 
-//user registration
+// user registration
 
-const registerUser = async(req,res)=>{
+const registerUser = async (req, res) => {
+
     logger.info('Registration endpoint hit...')
 
     try {
 
-        //validate the error
-        const {error} = validateRegistration(req.body)
+        // validate request body
 
-        if(error){
-            logger.warn('Validation error',error.details[0].message)
-                return res.status(400).json({
-                    success : false ,
-                    message : error.details[0].message
-                })
+        const { error } = validateRegistration(req.body)
 
-        }
-        const {email , password , username} = req.body
-        
-        let user = await User.findOne({ $or : [{email},{username}]})
-        if(user){
-            logger.warn('user already exist',error.details[0].message)
+        if (error) {
+
+            logger.warn('Validation error', error.details[0].message)
+
             return res.status(400).json({
-                    success : false ,
-                    message : "user already exist"
-                })
+                success: false,
+                message: error.details[0].message
+            })
         }
-        user = new user({username,email,password})
-        await user.save()
-        logger.warn("User saved successfully",user._id);
 
-        const {accessToken,refreshToken} = await  generateTokens(user)
+        const { email, password, username } = req.body
 
-        res.status(201).json({
-            success : true,
-            message : "user registered successfully!!",
-            accessToken,
-            refreshToken
+        // check existing user
+
+        let user = await User.findOne({
+            $or: [{ email }, { username }]
         })
+
+        if (user) {
+
+            logger.warn('user already exist')
+
+            return res.status(400).json({
+                success: false,
+                message: "user already exist"
+            })
+        }
+
+        // create new user
+
+        user = new User({
+            username,
+            email,
+            password
+        })
+
+        await user.save()
+
+        logger.info('User saved successfully', user._id)
+
+        // generate tokens
+
+        const { accessToken, refreshToken } = await generateTokens(user)
+
+        return res.status(201).json({
+            success: true,
+            message: "user registered successfully!!",
+            accessToken,
+            refreshToken,
+            user: {
+                id: user._id,
+                username: user.username,
+                email: user.email
+            }
+        })
+
     } catch (e) {
 
-        logger.error('Registration error occured',e)
-        res.status(500).json({
-            success : false,
-            message : "internal server error+"
+        logger.error('Registration error occured', e)
+
+        return res.status(500).json({
+            success: false,
+            message: e.message
         })
-        
     }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 module.exports = { registerUser }
